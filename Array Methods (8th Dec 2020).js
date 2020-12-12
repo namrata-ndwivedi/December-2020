b878afde743772d133552052d8b1c42e20b823d9
namrata-ndwivedi <73945159+namrata-ndwivedi@users.noreply.github.com>
2020-12-13 01:06:51 +0530

    Update Array Methods (8th Dec 2020).js

--- Array Methods (8th Dec 2020).js
+++ Array Methods (8th Dec 2020).js
@@ -1,3 +1,26 @@
+/*JS Refresh
+Content
+For each
+map
+filter
+reduce 
+some 
+find 
+every
+
+Solving problems using array functions on rest countries data.
+ - Get all the countries from Asia continent /region using Filter function 
+- Get all the countries with population of less than 2 lacs using Filter function
+ - Print the following details name, capital, flag using forEach function 
+- Print the total population of countries using reduce function 
+- Print the country which use US Dollars as currency.
+
+Meeting Recording: https://us02web.zoom.us/rec/share/ts39shZMpYCn5v64bwTlp0uvX4PzDWh1YoD3ofHCNi6FS5Va-tFOC4VTnqUpCURA.8HdKL5mL905mWrZs Access Passcode: B7m@fiat
+
+Session link: https://us02web.zoom.us/meeting/register/tZ0rdeytqjsuEt1VduXI3ri0R5ntjUiUajKJ Passcode : guvi
+*/
+
+
 //create a request variable 
 var request = new XMLHttpRequest();
 
